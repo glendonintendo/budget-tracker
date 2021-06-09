e8433e260d32cf75c7662acdd4881e19de1c528d
glendo <gleneggers64@gmail.com>
2021-06-09 11:09:40 -0500

    add offline form submission with fetch post on reconnect

---
+++ public/js/idb.js
@@ -0,0 +1,58 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(e) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget_item', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadBudgetItem();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveBudgetItem(budgetItem) {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+    const budgetItemObjectStore = transaction.objectStore('new_budget_item');
+    budgetItemObjectStore.add(budgetItem);
+}
+
+function uploadBudgetItem() {
+    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+    const budgetItemObjectStore = transaction.objectStore('new_budget_item');
+    const getAll = budgetItemObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_budget_item'], 'readwrite');
+                    const budgetItemObjectStore = transaction.objectStore('new_budget_item');
+                    budgetItemObjectStore.clear();
+                })
+                .catch(err => console.log(err));
+        }
+    };
+};
+
+window.addEventListener('online', uploadBudgetItem);

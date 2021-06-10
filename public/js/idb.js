8bd05516321f6c06c780e8d519855d4b907be16f
glendo <gleneggers64@gmail.com>
2021-06-10 07:14:26 -0500

    fix bug in onupgradeneeded event listener function

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,7 +2,7 @@ let db;
 
 const request = indexedDB.open('budget_tracker', 1);
 
-request.onupgradeneeded = function(e) {
+request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore('new_budget_item', { autoIncrement: true });
 };
